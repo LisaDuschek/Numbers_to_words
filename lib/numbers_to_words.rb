d48b4861e88839d2ee479899d9c16f950146cef3
Vaughn Dice & Lisa Duschek <student@epicodus.com>
2015-08-04 16:33:18 -0700

    refactor implementation logic to cut down code duplication

--- lib/numbers_to_words.rb
+++ lib/numbers_to_words.rb
@@ -1,6 +1,6 @@
 class Fixnum
   define_method(:numbers_to_words) do
-    single_digit_hash = {0 => 'zero',
+    $single_digit_hash = {0 => 'zero',
                          1 => 'one',
                          2 => 'two',
                          3 => 'three',
@@ -11,7 +11,7 @@ class Fixnum
                          8 => 'eight',
                          9 => 'nine'}
 
-    double_digit_hash = {0 => single_digit_hash,
+    $double_digit_hash = {0 => $single_digit_hash,
                          1 => {0 => 'ten',
                                1 => 'eleven',
                                2 => 'twelve',
@@ -31,68 +31,63 @@ class Fixnum
                          8 => 'eighty',
                          9 => 'ninety'}
 
-    number_string = self.to_s()
-    string_array = Array.new()
+    $number_string = self.to_s()
+    $string_array = Array.new()
 
-    while number_string.length() > 0 do
-      # do stuff
-      puts "number_string = #{number_string}"
-      puts "string_array = #{string_array}"
-      case number_string.length()
-      when 1
-        word = single_digit_hash.fetch(number_string[0].to_i())
-        string_array.push(word) unless word.eql?('zero')
-      when 2
-        first_word = double_digit_hash.fetch(number_string[0].to_i())
+    def handle_one_digit_number()
+      word = $single_digit_hash.fetch($number_string[0].to_i())
+      $string_array.push(word) unless word.eql?('zero')
+    end
 
-        if number_string[0] == '0' || number_string[0] == '1' # do not need to fetch second word
-          first_word = first_word.fetch(number_string[1].to_i())
-          string_array.push(first_word)
-        else  # need to fetch second word
-          second_word = single_digit_hash.fetch(number_string[1].to_i())
-          string_array.push(first_word)
-          string_array.push(second_word) unless second_word.eql?('zero')
+    def handle_two_digit_number()
+      first_word = $double_digit_hash.fetch($number_string[0].to_i())
+
+      if $number_string[0] == '0' || $number_string[0] == '1' # do not need to fetch second word
+        first_word = first_word.fetch($number_string[1].to_i())
+        $string_array.push(first_word)
+      else  # need to fetch second word
+        second_word = $single_digit_hash.fetch($number_string[1].to_i())
+        $string_array.push(first_word)
+        $string_array.push(second_word) unless second_word.eql?('zero')
+      end
+    end
+
+    def handle_three_digit_number(type)
+      if !$string_array.include?(type)
+        first_word = $single_digit_hash.fetch($number_string[0].to_i())
+        if !first_word.eql?('zero')
+          second_word = type
+          $string_array.push([first_word, second_word])
         end
+      end
+    end
+
+    # main loop
+    while $number_string.length() > 0 do
+
+      case $number_string.length()
+      when 1
+        handle_one_digit_number()
+      when 2
+        handle_two_digit_number()
         break
       when 3
-        first_word = single_digit_hash.fetch(number_string[0].to_i())
-        if !first_word.eql?('zero')
-          second_word = "hundred"
-          string_array.push([first_word, second_word])
-        end
+        handle_three_digit_number("hundred")
       when 4
-        if !string_array.include?("thousand")
-          first_word = single_digit_hash.fetch(number_string[0].to_i())
-          second_word = "thousand"
-          string_array.push([first_word, second_word])
-        end
+        handle_three_digit_number("thousand")
       when 5
-        # TODO: remove duplication
-        first_word = double_digit_hash.fetch(number_string[0].to_i())
-
-        if number_string[0] == '0' || number_string[0] == '1' # do not need to fetch second word
-          first_word = first_word.fetch(number_string[1].to_i())
-          string_array.push(first_word)
-        else  # need to fetch second word
-          string_array.push(first_word)
-          string_array.push(single_digit_hash.fetch(number_string[1].to_i()))
-        end
-        string_array.push("thousand")
+        handle_two_digit_number()
+        $string_array.push("thousand")
       when 6
-        if !string_array.include?("thousand")
-          #first_word = single_digit_hash.fetch(number_string[0].to_i())
-          first_word = single_digit_hash.fetch(number_string[0].to_i())
-          second_word = "hundred"
-          string_array.push([first_word, second_word])
-        end
+        handle_three_digit_number("hundred")
       else
         # don't do anything
       end
 
       # chop off analyzed digit
-      number_string.replace number_string[1..-1]
+      $number_string.replace $number_string[1..-1]
     end
 
-    string_array.join(' ')
+    $string_array.join(' ')
   end
 end

18637bc992c5eceec48748c55d90515f85840fd9
Vaughn Dice & Lisa Duschek <student@epicodus.com>
2015-08-04 14:55:09 -0700

    It will convert double digit number to written form

--- lib/numbers_to_words.rb
+++ lib/numbers_to_words.rb
@@ -1,43 +1,60 @@
 class Fixnum
   define_method(:numbers_to_words) do
-    digit_hash = {0 => 'zero',
-                  1 => 'one',
-                  2 => 'two',
-                  3 => 'three',
-                  4 => 'four',
-                  5 => 'five',
-                  6 => 'six',
-                  7 => 'seven',
-                  8 => 'eight',
-                  9 => 'nine'}
+    single_digit_hash = {0 => 'zero',
+                         1 => 'one',
+                         2 => 'two',
+                         3 => 'three',
+                         4 => 'four',
+                         5 => 'five',
+                         6 => 'six',
+                         7 => 'seven',
+                         8 => 'eight',
+                         9 => 'nine'}
 
-                        #  10 => 'ten',
-                        #  11 => 'eleven',
-                        #  12 => 'twelve',
-                        #  13 => 'thirteen',
-                        #  14 => 'fourteen',
-                        #  15 => 'fifteen',
-                        #  16 => 'sixteen',
-                        #  17 => 'seventeen',
-                        #  18 => 'eighteen',
-                        #  19 => 'nineteen',
-                        #  20 => 'twenty',
-                        #  30 => 'thirty',
-                        #  40 => 'forty',
-                        #  50 => 'fifty',
-                        #  60 => 'sixty'
-                        #  }
+    double_digit_hash = {1 => {0 => 'ten',
+                               1 => 'eleven',
+                               2 => 'twelve',
+                               3 => 'thirteen',
+                               4 => 'fourteen',
+                               5 => 'fifteen',
+                               6 => 'sixteen',
+                               7 => 'seventeen',
+                               8 => 'eighteen',
+                               9 => 'nineteen'},
+                         2 => 'twenty',
+                         3 => 'thirty',
+                         4 => 'forty',
+                         5 => 'fifty',
+                         6 => 'sixty',
+                         7 => 'seventy',
+                         8 => 'eighty',
+                         9 => 'ninety'}
 
-    # length_hash = {1 => single_digit_hash,
-    #                2 => double_digit_hash}
+    number_string = self.to_s()
+    number_length = number_string.length()
 
-    # 10 = ten
-    # 15 = fifteen
-    # 20 = twenty
-    # 145 = One Hundred Forty Five
-    #self.split().each() do |digit|
+    # number_string.each_char() do |char|
+    #   index_of_char = number_string.index(char)
+    #
+    #   puts "char: #{char} at index: #{index_of_char}"
+    # end
 
-    digit_hash.fetch(self)
+    string_array = Array.new()
+    if number_length == 1
+      string_array.push(single_digit_hash.fetch(self))
+    elsif number_length == 2
+      first_char = double_digit_hash.fetch(number_string[0].to_i())
+
+      if number_string[0] == '1' # do not need to fetch second char
+        first_char = first_char.fetch(number_string[1].to_i())
+        string_array.push(first_char)
+      else  # need to fetch second char
+        string_array.push(first_char)
+        string_array.push(single_digit_hash.fetch(number_string[1].to_i()))
+      end
+
+    end
+    string_array.join(' ')
 
   end
 end
--- spec/numbers_to_words_spec.rb
+++ spec/numbers_to_words_spec.rb
@@ -2,7 +2,16 @@ require ('rspec')
 require('numbers_to_words')
 
 describe('Fixnum#numbers_to_words')do
-   it('converts one digit to written form')do
-     expect(2.numbers_to_words()).to(eq('two'))
- end
+  it('converts one digit to written form')do
+   expect(2.numbers_to_words()).to(eq('two'))
+  end
+
+  it('converts two digits to written form')do
+    expect(99.numbers_to_words()).to(eq('ninety nine'))
+  end
+
+  it('converts two digits to written form if teen')do
+    expect(17.numbers_to_words()).to(eq('seventeen'))
+  end
+
 end
